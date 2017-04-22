20ad2b7945cda4c3ae61da1f72696aafc5bbc6c0
rahulrajewar <rahulrajewar94217@gmail.com>
2017-04-21 21:23:50 -0400

    Might have some errors

--- e.py
+++ e.py
@@ -1,6 +1,8 @@
 import urllib
 import requests
 import json
+import time
+import os
 
 def topology_info():
 	print '\n<----------------------------SUMMARY------------------------------>'
@@ -21,7 +23,7 @@ def switch_info ( number_of_switches , switch_dpids ):
 		DPID = dat [k]["switchDPID"]
 		DPID = DPID.decode()
 		switch_dpids.append(DPID)
-	return(switch_dpids)
+	return (switch_dpids)
 
 def host_info(number_of_hosts , hosts):
 	a = requests.get("http://10.0.2.15:8080/wm/device/")
@@ -41,24 +43,69 @@ def switch_byte( number_of_switches, switch_dpids ):
 		print '\t Cookie:', b["flows"][0]['cookie']
 		print '\t byte count:', b["flows"][0]['byte_count']
 
+def bandwidth ( switch_dpids, number_of_switches ):
+	for k in range(	0, number_of_switches ):
+		print switch_dpids[k]
+		for port in range(1,5):
+			try:				
+				a = requests.get("http://10.0.2.15:8080/wm/statistics/bandwidth/"+switch_dpids[k]+"/"+str(port)+"/json")
+				b = a.json()
+				print "\tport :", b[0]["port"]
+				bandwidth = b[0]["bits-per-second-rx"]
+				print "\t\tBits per Second :", bandwidth
+				if bandwidth > 15000:
+					print 'adding static flow rule:'
+					one = 'curl -X POST -d'
+					two = '{"switch":"', switch_dpid[k]
+					three = ', "name":"flow-mode-', k
+					four = '", "cookie":"0", "priority":"32768", "in_port":"',port
+					five = '", "active":"true", "actions":"no-forward"}'
+					six = ' http://10.0.2.15:8080/wm/staticentrypusher/json'
+					print one,two,three,four,five,six
+					os.system(one,two,three,four,five,six)
+					print 'flow rule added'
+			except:
+				print "port", port," no connected"
+
+def stat_enable():
+
+	try:
+		stat = requests.put("http://10.0.2.15:8080/wm/statstics/config/enable/json")
+		print "Enabled statistics..."
+
+	except Exception as e:
+		print"error while enabling statistics :" , e
+
+def start():
+
+	try:
+
+		number_of_switches , number_of_hosts = topology_info();
+		switch_dpids = list()
+		hosts = dict()
+		switch_dpids = switch_info ( number_of_switches, switch_dpids ) ;
+		hosts = host_info ( number_of_hosts , hosts );
+		switch_byte ( number_of_switches, switch_dpids );			
+		bandwidth ( switch_dpids, number_of_switches );
+
+	except Exception as e:
+		print'Error occured', e
+
 try:
-	number_of_switches , number_of_hosts = topology_info();
-	switch_dpids = list()
-	hosts = dict()
-	switch_dpids = switch_info ( number_of_switches, switch_dpids ) ;
-	hosts = host_info ( number_of_hosts , hosts );
-	switch_byte( number_of_switches, switch_dpids );
 
-except Exception as e:
-	print'Error occured', e
+	stat_enable();
+	while 1:
+		secs = 5
+		time.sleep(secs)
+		start()
 
-
+except:
+	print'Errorrrrrrrrrrrr'
 
 #to access hosts info from dict
-'''
 
-	for k in range( 1, number_of_hosts-1):
-		k = str(k)
-		m = "[u'10.0.0."+ k +"']"
-		print hosts[m]
-'''
+
+#	for k in range( 1, number_of_hosts-1):
+#		k = str(k)
+#		m = "[u'10.0.0."+ k +"']"
+#		print hosts[m]
